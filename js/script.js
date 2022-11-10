d203f6b34096736b01568a6404eef20c17dc3dc7
Adanel76 <97354517+Adanel76@users.noreply.github.com>
2022-11-10 15:58:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,189 +1,241 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.headerNavigation')
+const them = document.querySelector('.btnThem')
+const more = document.querySelector('.more')
+const dropInfo = document.querySelector('.downInfo')
+const dropPanel = document.querySelector('.dropPanel')
+const headerLink = document.querySelector('.headerLinkMore')
+const linkDrop = document.querySelectorAll('.linkDrop')
+const navAlt = document.querySelector('.navAlt')
+const hamburger = document.querySelector('.hamburger')
+const themAlt = document.querySelector('.btnThemAlt')
+const dropInfoAlt = document.querySelector('.dropInfoAlt')
+const linkAltDrop = document.querySelector('.linkAltDrop')
+const dropLinkAltBack = document.querySelector('.dropLinkAltBack')
+const main = document.querySelector('.main')
+const navAltAnimation = document.querySelector('.navAltAnimation')
+const head = document.querySelector('.head')
+const title = document.querySelector('.title')
+const titleAlt = document.querySelector('.titleAlt')
+const generalInfo = document.querySelector('.generalInfo')
+const generalInfoAlt = document.querySelector('.generalInfoAlt')
+const geographyInfo = document.querySelector('.geographyInfo')
+const hightlights = document.queryCommandIndeterm('.hightlights')
+const climate = document.querySelector('.climate')
+const bestTime = document.querySelector('.bestTime')
+const footer = document.querySelector('.footer')
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
+
+
+if (!localStorage.them) localStorage.them = 'light'
+document.body.classList = localStorage.them
+them.innerText =  document.body.classList.contains('dark') ? 'Светлая тема' : 'Тёмная тема'
+
+them.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+    them.innerText =  document.body.classList.contains('dark') ? 'Светлая тема' : 'Тёмная тема'
+    localStorage.them = document.body.classList || 'light'
+})
+
+
+if (!localStorage.themAlt) localStorage.themAlt = 'light'
+document.body.classList = localStorage.themAlt
+themAlt.innerText =  document.body.classList.contains('dark') ? 'Светлая тема' : 'Тёмная тема'
+
+themAlt.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+    themAlt.innerText =  document.body.classList.contains('dark') ? 'Светлая тема' : 'Тёмная тема'
+    localStorage.themAlt = document.body.classList || 'light'
+})
+
+
+const hamburgerClick = () => {
+    isnavAlt = navAlt.classList.toggle('navAltAnimation')
+    isBool = hamburger.classList.toggle('hambugerActive')
+    
+    if (isnavAlt) {
+        hamburger.style.backgroundColor = '#000'
+    } else {
+        let scrollPos = window.scrollY
+
+        if (scrollPos > 1) {
+            hamburger.style.backgroundColor = '#000'
+        } else {
+            hamburger.style.backgroundColor = 'rgba(0, 0, 0, .4)'
+        }
+    }
+
+}
+
+hamburger.addEventListener('click', hamburgerClick)
+
+document.addEventListener('DOMContentLoaded', () => { // Структура страницы загружена и готова к взаимодействию
+
+    window.addEventListener('click', e => { // при клике в любом месте окна браузера
+      const target = e.target // находим элемент, на котором был клик
+      if (!target.closest('.navAlt') && !target.closest('.hamburger') && !target.closest('.dropInfoAlt')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+        navAlt.classList.remove('navAltAnimation') // то закрываем окно навигации, удаляя активный класс
+        hamburger.classList.remove('hambugerActive') // то закрываем окно навигации, удаляя активный класс
+        dropInfoAlt.classList.remove('dropInfoAltAnimation') // то закрываем окно навигации, удаляя активный класс
+      }
     })
-});
-
-$('#card-text-id-first').typeIt({
-    speed: 70, //скорость анимации
-    autoStart: false /*если true то анимация начнется сразу после загрузки страницы, если false, то только когда блок будет в зоне видимости*/
-}); 
-
-$('#card-text-id-second').typeIt({
-    speed: 70, //скорость анимации
-    autoStart: false /*если true то анимация начнется сразу после загрузки страницы, если false, то только когда блок будет в зоне видимости*/
-}); 
-
-$('#card-text-id-third').typeIt({
-    speed: 70, //скорость анимации
-    autoStart: false /*если true то анимация начнется сразу после загрузки страницы, если false, то только когда блок будет в зоне видимости*/
-}); 
-
-
-
-
-
-$('.info_header_one').typeIt({
-    speed: 100,
-    autoStart: false,
-    
-})
-.tiPause(70)
-.tiType(']')
-.tiPause(70)
-.tiDelete(1)
-.tiType('т')
-.tiPause(70)
-.tiType('#')
-.tiPause(70)
-.tiDelete(1)
-.tiType('а')
-.tiPause(70)
-.tiType('%')
-.tiPause(70)
-.tiDelete(1)
-.tiType('п')
-.tiPause(70)
-.tiType('&')
-.tiPause(70)
-.tiDelete(1)
-.tiType(' ')
-.tiPause(70)
-.tiType('@')
-.tiPause(70)
-.tiDelete(1)
-.tiType('1');
-
-$('.info_header_two').typeIt({
-    speed: 100,
-    autoStart: false
-})
-.tiPause(70)
-.tiType('#')
-.tiPause(70)
-.tiDelete(1)
-.tiType('т')
-.tiPause(7)
-.tiType('/')
-.tiPause(7)
-.tiDelete(1)
-.tiType('а')
-.tiPause(70)
-.tiType('^')
-.tiPause(7)
-.tiDelete(1)
-.tiType('п')
-.tiPause(70)
-.tiType('{')
-.tiPause(7)
-.tiDelete(1)
-.tiType(' ')
-.tiPause(70)
-.tiType('}')
-.tiPause(70)
-.tiDelete(1)
-.tiType('2');
-
-$('.smart_string_one').typeIt({
-    speed: 100,
-    autoStart: false
-})
-.tiPause(70)
-.tiType(']')
-.tiPause(70)
-.tiDelete(1)
-.tiType('т')
-.tiPause(70)
-.tiType('#')
-.tiPause(70)
-.tiDelete(1)
-.tiType('а')
-.tiPause(70)
-.tiType('%')
-.tiPause(70)
-.tiDelete(1)
-.tiType('п')
-.tiPause(70)
-.tiType('&')
-.tiPause(70)
-.tiDelete(1)
-.tiType(' ')
-.tiPause(70)
-.tiType('@')
-.tiPause(70)
-.tiDelete(1)
-.tiType('1');
-
-$('.smart_string_two').typeIt({
-    speed: 100,
-    autoStart: false
-})
-.tiPause(70)
-.tiType('#')
-.tiPause(70)
-.tiDelete(1)
-.tiType('т')
-.tiPause(70)
-.tiType('/')
-.tiPause(70)
-.tiDelete(1)
-.tiType('а')
-.tiPause(70)
-.tiType('^')
-.tiPause(70)
-.tiDelete(1)
-.tiType('п')
-.tiPause(70)
-.tiType('{')
-.tiPause(70)
-.tiDelete(1)
-.tiType(' ')
-.tiPause(70)
-.tiType('}')
-.tiPause(70)
-.tiDelete(1)
-.tiType('2');
-
-AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-    
   
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 1000, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-});
+  })
 
-if(!localStorage.theme) localStorage.theme = "dark"
-document.body.className = localStorage.theme
-changeThemBtn.innerText = document.body.classList.contains("white") ? "Светлая тема" : "Тёмная тема"
 
-changeThemBtn.onclick = () => {
-    document.body.classList.toggle("white")
-    changeThemBtn.innerText = document.body.classList.contains("white") ? "Светлая тема" : "Тёмная тема"
-    localStorage.theme = document.body.className || "dark"
-};
+
+const scrollWatch = () => {
+    let scrollPos = window.scrollY
+
+    if (scrollPos > 1) {
+        header.classList.add('titleBlack')
+    } else {
+        header.classList.remove('titleBlack')
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', scrollWatch)
+
+window.addEventListener('scroll', scrollWatch)
+
+
+
+const clickWatch = () => {
+    isDropBool = dropInfo.classList.toggle('dropPanel')
+
+    const dropTimeTrue = () => {
+        if (isDropBool) {
+            for (let i = 0; i < linkDrop.length; i++) {
+                linkDrop[i].style.display = 'block'
+            }
+        } 
+    }
+
+    const dropTimeFalse = () => {
+        if (isDropBool === false) {
+            for (let i = 0; i < linkDrop.length; i++) {
+                linkDrop[i].style.display = 'none'
+            }
+        }
+    }
+
+    setTimeout(dropTimeTrue, 300)
+
+    setTimeout(dropTimeFalse, 100)
+}
+
+more.addEventListener('click', clickWatch)
+
+
+
+const moreTopWatch = () => {
+    headerLink.classList.toggle('headerLinkTop')
+}
+
+more.addEventListener('click', moreTopWatch)
+
+
+
+click = 0
+
+const linkClick = () => {
+    click += 1
+    
+    if (click % 2 === 0) {
+        headerLink.style.borderRadius = '10px'
+        headerLink.style.transition = '0.5s all ease-in-out'
+    } else {
+        headerLink.style.borderRadius = '10px 10px 0 0'
+        headerLink.style.transition = '0.2s all ease-in-out'
+    }
+}
+
+more.addEventListener('click', linkClick)
+
+
+
+const hamburgerScroll = () => {
+    let scrollPos = window.scrollY
+
+    if (scrollPos > 1) {
+        hamburger.style. backgroundColor = '#000'
+    } else {
+        hamburger.style. backgroundColor = 'rgba(0, 0, 0, .4)'
+    }
+}
+
+window.addEventListener('scroll', hamburgerScroll)
+
+
+
+const linkAltDropClick = () => {
+    dropInfoAlt.classList.add('dropInfoAltAnimation')
+}
+
+linkAltDrop.addEventListener('click', linkAltDropClick)
+
+
+const linkAltDropBackClick = () => {
+    dropInfoAlt.classList.remove('dropInfoAltAnimation')
+}
+
+dropLinkAltBack.addEventListener('click', linkAltDropBackClick) 
+
+
+
+const hamburgerBackClick = () => {
+    dropInfoAlt.classList.remove('dropInfoAltAnimation')
+}
+
+hamburger.addEventListener('click', hamburgerBackClick)
+
+
+
+
+
+const logBlock = document.querySelector('.main')
+
+let x1 = null
+let y1 = null
+
+const handleTouchStart = (event) => {
+    const firstTouch = event.touches[0]
+
+    x1 = firstTouch.clientX
+    y1 = firstTouch.clientY
+}
+
+const handleTouchMove = (event) => {
+    if (!x1 || !y1) {
+        return false
+    }
+    let x2 = event.touches[0].clientX
+    let y2 = event.touches[0].clientY
+
+    let xDiff = x2 - x1
+    let yDiff = y2 - y1
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            navAlt.classList.add('navAltAnimation')
+            hamburger.classList.add('hambugerActive')
+        } else {
+            navAlt.classList.remove('navAltAnimation')
+            hamburger.classList.remove('hambugerActive')
+            dropInfoAlt.classList.remove('dropInfoAltAnimation')
+        }
+    } else {
+        if (yDiff > 0) {
+            console.log('down')
+        } else {
+            console.log('top')
+        }
+    }
+
+    x1 = null
+    y1 = null
+}
+
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchmove', handleTouchMove, false)
